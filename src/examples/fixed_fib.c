107e72fb89209cf04d01742e8e73148b999c8dc9
Mihailo Isakov <mihailo@protonmail.com>
2017-02-27 11:59:58 -0500

    updated example

--- src/examples/fixed_fib.c
+++ src/examples/fixed_fib.c
@@ -1,17 +1,20 @@
+#include "stdio.h"
+
 int main() {
 
     int a = 1, b = 1, c = 0;
 
-    int n = 100;
+    int n = 1000;
     
     if (n == 0 || n == 1) {
         return 1;
     }
 
     for (int i = 2; i <= n; i++) {
-        c = a + b;
+        c = (a + b);
         b = a;
         a = c;
+        printf("%d\n", c);
     }
 
     return c;
