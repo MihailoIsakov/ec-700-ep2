1ea6de32ff8b437c75b9bd17c3bc2c94250c14a7
Mihailo Isakov <mihailo@protonmail.com>
2017-02-27 12:02:30 -0500

    dictionary storing disasembly and categories

--- src/pintools/test.cpp
+++ src/pintools/test.cpp
@@ -4,32 +4,23 @@
 #include <bitset>
 #include <string>
 #include <iomanip>
+#include <map>
 
 #define TAINT_ARRAY_SIZE 100
 #define FLAGS_REG_INDEX  25
-
 bool flag = true;
 ofstream RegValuesFile; 
-ofstream RegNamesFile; 
 int instr_count = 0;
 
 bool taint_array[TAINT_ARRAY_SIZE];
+std::map<ADDRINT, std::string> disAssemblyMap;
+std::map<ADDRINT, std::string> categoryMap;
+
 
 int get_bit(int value, int n) {
     // returns nonzero value if the nth bit is set
     // input n is zero-indexed
     return (int) (value & (1 << n)) != 0;
-}
-
-VOID printReg(UINT64 addr, REG reg, ADDRINT value, INS ins) { 
-    RegValuesFile << std::left << std::setw(16) << addr 
-                  << ": " << std::setw(32) << INS_Disassemble(ins) << "; " 
-                  << std::setw(6) << REG_StringShort(reg) << " = " << std::setw(20) << value;
-
-    if (REG_StringShort(reg).compare("rflags") == 0 && get_bit(value, 11))
-        RegValuesFile << " OVERFLOW!" << endl;
-    else
-        RegValuesFile << endl;
 }
 
 bool taint1_policy(INS ins, bool reg1_taint) {
@@ -46,46 +37,58 @@ bool taint2_policy(INS ins, bool reg1_taint, bool reg2_taint) {
 // We are interested in arithmetic operations, 
 // excluding CMP which performs subtractions causing OFs
 bool can_overflow(INS ins) {
-    bool can = true;
     if (CATEGORY_StringShort(INS_Category(ins)) != "BINARY")
-        can = false;
+        return false;
     if (INS_Mnemonic(ins) == "CMP")
-        can = false;
-    return can;
+        return false;
+    return true;
 }
 
-VOID taint(INS ins, REG flags) { 
-    //const UINT32 max_w = INS_MaxNumWRegs(ins);
+// TODO add check if flags is being written to in this instruction
+VOID taint(ADDRINT ip, INS ins, REG flags) { 
 
     bool taint = false;
     if (can_overflow(ins))
         taint = get_bit(flags, 11);
 
-    if (taint) {
-        cout << INS_RegW(ins, 0) << endl;
-        taint_array[INS_RegW(ins, 0)] = taint;
-    }
-    else {
-        // depending on the number of operands, use different propagation function
-        const UINT32 max_r = INS_MaxNumRRegs(ins); 
-    
-        switch(max_r) {
-            case 1: {
-                REG read = INS_RegR(ins, 0);
-                taint = taint1_policy(ins, taint_array[read]);
-            }
-            case 2: {
-                REG read1 = INS_RegR(ins, 0);
-                REG read2 = INS_RegR(ins, 1);
-                taint = taint2_policy(ins, taint_array[read1], taint_array[read2]);
-            }
-        }
-        
-        REG write = INS_RegW(ins, 0);
-        taint_array[write] = taint;
-    }
+    //if (taint) {
+        //REG write_to = INS_RegW(ins, 0);
+        //write_to = REG_FullRegName(write_to); // (EAX, AX, AH, AL) -> RAX
 
-    RegValuesFile << std::left << std::setw(36) << INS_Disassemble(ins) << "; ";
+        //if (REG_is_gr(write_to))
+            //taint_array[write_to] = taint;
+    //}
+    //else {
+        //// depending on the number of operands, use different propagation function
+        //const UINT32 max_r = INS_MaxNumRRegs(ins); 
+    
+        //switch(max_r) {
+            //case 1: {
+                //REG read = INS_RegR(ins, 0);
+                //taint = taint1_policy(ins, taint_array[read]);
+                //if(taint)
+                    //cout << "1: "<< INS_Disassemble(ins) << endl;
+                //break;
+            //}
+            //case 2: {
+                //REG read1 = INS_RegR(ins, 0);
+                //REG read2 = INS_RegR(ins, 1);
+                //taint = taint2_policy(ins, taint_array[read1], taint_array[read2]);
+                //if(taint)
+                    //cout << "2: "<< INS_Disassemble(ins) << endl;
+                //break;
+            //}
+            //default: {
+                //break;
+            //}
+        //}
+        
+        //REG write = INS_RegW(ins, 0);
+        //taint_array[write] = taint;
+    //}
+    
+
+    RegValuesFile << std::left << ip << " " << std::setw(10) << std::hex << categoryMap[ip] << " " << std::setw(36) << disAssemblyMap[ip] << "; ";
     if (taint)
         RegValuesFile << "OF ";
     else
@@ -100,10 +103,10 @@ VOID taint(INS ins, REG flags) {
             RegValuesFile << "0";
         f >>= 1;
     }
-    RegValuesFile << "\t";
-    for (int i=0; i<TAINT_ARRAY_SIZE; i++) {
-        RegValuesFile << (taint_array[i] != 0);
-    }
+    //RegValuesFile << "\t";
+    //for (int i=0; i<TAINT_ARRAY_SIZE; i++) {
+        //RegValuesFile << (taint_array[i] != 0);
+    //}
     RegValuesFile << endl;
 }
 
@@ -113,20 +116,19 @@ VOID Instruction(INS ins, VOID *v)
     //const UINT32 max_r=INS_MaxNumRRegs(ins);//number of readed registers in the instruction
     //const UINT32 max_w=INS_MaxNumWRegs(ins);//number of written registers in the instruction
 
-
     if(flag){    // initialize taint array for just once
-
+        flag = false;
         RegValuesFile.open("logs/values.out");
-        RegNamesFile.open("logs/names.out");
-
-        flag=false;
-
         for (int i=0; i<TAINT_ARRAY_SIZE; i++)
             taint_array[i] = 0;
     }
+
+    disAssemblyMap[INS_Address(ins)] = INS_Disassemble(ins);
+    categoryMap[INS_Address(ins)] = CATEGORY_StringShort(INS_Category(ins));
     
     INS_InsertCall(ins, IPOINT_BEFORE, //this might be IPOINT_AFTER, we might need to check FLAGS after ins execution done 
         (AFUNPTR) taint,
+        IARG_INST_PTR,
         IARG_UINT32, ins,
         IARG_REG_VALUE, FLAGS_REG_INDEX,
         IARG_END);
@@ -136,10 +138,8 @@ VOID Instruction(INS ins, VOID *v)
 VOID Fini(INT32 code, VOID *v)
 {
     RegValuesFile.setf(ios::showbase);
-    RegNamesFile.setf(ios::showbase);
 
     RegValuesFile.close();
-    RegNamesFile.close();
 }
 
 /* ===================================================================== */
