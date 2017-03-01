aeec706e32e8b8c373eec3900586de10b01ab594
Mihailo Isakov <mihailo@protonmail.com>
2017-03-01 00:03:06 -0500

    moved malloctracting

--- src/pintools/pintests/malloctracing.cpp
+++ src/pintools/pintests/malloctracing.cpp
@@ -25,8 +25,6 @@
 
 
 std::ofstream TraceFile;
-std::ofstream TraceFile2;
-std::ofstream TraceFile3;
 
 bool flag=true;
 
@@ -54,40 +52,44 @@ VOID MallocAfter(ADDRINT ret)
 
 }
 
-VOID Arg1Memcpy(CHAR * name, ADDRINT size,ADDRINT arg1,ADDRINT arg2)
+VOID Arg1Memcpy(CHAR * name, ADDRINT size, ADDRINT arg1,ADDRINT arg2)
 {
 	TraceFile << name << "(" << size << ")" << "(" << arg1 << ")" <<"(" << arg2 << ")" <<endl;
 	std::cout<<"Arg1MemcpyBefore"<<endl;
 }
 
-VOID Routine  (RTN rtn,VOID *v){
-
-	TraceFile3<<RTN_Name(rtn)<<endl;
-
-
-}
 
 VOID Image(IMG img, VOID *v)
 {
+
+	if(flag){    // initialize taint array for just once
+
+		//OutFile.setf(ios::showbase);//open file
+		TraceFile.setf(ios::app | ios::out);
+		TraceFile.open("/disk/logs/mallocsado.out");
+
+		flag=false;
+	}
 	// Instrument the malloc() and free() functions.  Print the input argument
 	// of each malloc() or free(), and the return value of malloc().
 
 	std::cout<< IMG_Name(img)<<endl;	//
 	//  Find the malloc() function.
 	RTN mallocRtn = RTN_FindByName(img, MALLOC);
-	if (RTN_Valid(mallocRtn))
-	{
+	if (RTN_Valid(mallocRtn)) {
 		RTN_Open(mallocRtn);
 		//std::cout<<"MALLOC:"<<INS_Disassemble(img)<<endl;
-		//		TraceFile2<<"IMAGE"<<endl;
 		//Mihailo: Check if the RDI is tainted and if that is the case EXCEPTION!!!!!!!
 		// Instrument malloc() to print the input argument value and the return value.
-		RTN_InsertCall(mallocRtn, IPOINT_BEFORE, (AFUNPTR)Arg1Before,
+		RTN_InsertCall(mallocRtn, IPOINT_BEFORE, (AFUNPTR) Arg1Before,
 				IARG_ADDRINT, MALLOC,
 				IARG_FUNCARG_ENTRYPOINT_VALUE, 0,
 				IARG_END);
-		RTN_InsertCall(mallocRtn, IPOINT_AFTER, (AFUNPTR)MallocAfter,
-				IARG_FUNCRET_EXITPOINT_VALUE, IARG_END);
+
+		RTN_InsertCall(mallocRtn, 
+                IPOINT_AFTER, (AFUNPTR)MallocAfter,
+				IARG_FUNCRET_EXITPOINT_VALUE, 
+                IARG_END);
 
 		RTN_Close(mallocRtn);
 	}
@@ -128,27 +130,6 @@ VOID Image(IMG img, VOID *v)
 // Instrumentation routine: for each instruction call this function
 //////////////////////////////////////////////////////////////////////
 
-VOID Instruction(INS ins, VOID *v)
-{
-	TraceFile2<<"INS:"<<INS_Disassemble(ins)<<endl;
-
-	if(flag){    // initialize taint array for just once
-
-		//OutFile.setf(ios::showbase);//open file
-		TraceFile.setf(ios::app | ios::out);
-		TraceFile.open("outputfiles/mallocsado.out");
-
-		TraceFile2.setf(ios::app | ios::out);
-		TraceFile2.open("outputfiles/mallocsado2.out");
-		TraceFile3.setf(ios::app | ios::out);
-		TraceFile3.open("outputfiles/mallocsado3.out");
-
-
-		flag=false;
-	}
-
-
-}
 
 INT32 Usage()
 {
@@ -163,8 +144,6 @@ VOID Fini(INT32 code, VOID *v)
 {
 	// Write to a file since cout and cerr maybe closed by the application
 	TraceFile.close();
-	TraceFile2.close();
-	TraceFile3.close();
 }
 
 
@@ -181,10 +160,6 @@ int main(int argc, char * argv[])
 	//PIN_InitSymbolsAlt():
 	if (PIN_Init(argc, argv)) return Usage();
 
-	INS_AddInstrumentFunction(Instruction, 0);
-	// Register Image to be called to instrument functions.
-
-	RTN_AddInstrumentFunction(Routine, 0);	
 	IMG_AddInstrumentFunction(Image, 0);
 	// Register Fini to be called when the application exits
 	PIN_AddFiniFunction(Fini, 0);
