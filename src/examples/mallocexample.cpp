1ccfa1d70bf2d4b187b0d3caf161a47c25af7c71
Mihailo Isakov <mihailo@protonmail.com>
2017-03-01 00:02:23 -0500

    updated example

--- src/examples/mallocexample.cpp
+++ src/examples/mallocexample.cpp
@@ -1,31 +1,19 @@
-
-#include <stdio.h>      /* printf, scanf, NULL */
+#include <stdio.h>
 #include <stdlib.h>     /* malloc, free, rand */
 
 int main ()
 {
-short  int i,n,k;
-  char * buffer;
+    int i,n,k;
+    char * buffer;
 
-i=32000;
-k=768;
+    i=1500000000;
+    //i = 100;
+    k=1500000000;
 
-short int mallocinput=i+k;
-  printf("input is %d \n",mallocinput);
-  printf ("How long do you want the string?\n ");
-  printf( "%d should be fine \n ",mallocinput); 
-// scanf ("%d", &i);
+    int mallocinput=i+k;
+    buffer = (char*) malloc (mallocinput+1);
 
-  buffer = (char*) malloc (mallocinput+1);
-  if (buffer==NULL) exit (1);
+    printf("Successfully finished!\n");
 
-  for (n=0; n<i; n++)
-    buffer[n]=rand()%26+'a';
-  buffer[i]='\0';
-
-  printf ("Random string: %s\n",buffer);
-  free (buffer);
-
-  return 0;
-
+    return 0;
 }
