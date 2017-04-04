11a43916664c4eb05e85e95f58e5dfe95fd80afb
Mihailo Isakov <mihailo@protonmail.com>
2017-04-04 17:18:20 -0400

    scritps

---
+++ src/dump_mem.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# Script runs the string compare pintool on a binary provided in the first parameter, 
+# and saves the results to the file provided in the second parameter. The pintool dumps the memory
+# every time it hits on a function set in pintools/strcmp_dump.cpp, which in our case is the compare
+# function in the class string, from the standard library.
+
+$PIN_ROOT/pin -t pintools/obj-intel64/strcmp_dump.so -- $1 > $2
